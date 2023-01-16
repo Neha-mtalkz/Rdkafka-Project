cb022cee5c15ad7fa530e6d7cad36576906df0c6
Neha-mtalkz <neha.yadav@mtalkz.com>
2023-01-16 11:55:55 +0530

    add multiple consumers

--- consumer.js
+++ consumer.js
@@ -5,11 +5,11 @@ const consumer1 = new kafka.KafkaConsumer({
     "group.id": "hello-group",
     "auto.offset.reset": "earliest"
 });
-// const consumer2 = new kafka.KafkaConsumer({
-//     "metadata.broker.list": "127.0.0.1:9092",
-//     "group.id": "myid",
-//     "auto.offset.reset": "earliest"
-// });
+const consumer2 = new kafka.KafkaConsumer({
+    "metadata.broker.list": "127.0.0.1:9092",
+    "group.id": "hello-group",
+    "auto.offset.reset": "earliest"
+});
 
 // const consumer3 = new kafka.KafkaConsumer({
 //     "metadata.broker.list": "127.0.0.1:9092",
@@ -19,7 +19,7 @@ const consumer1 = new kafka.KafkaConsumer({
 
 consumer1.on("ready", function () {
     consumer1.subscribe(['new-topic']);
-    consumer1.consume();
+    consumer1.consume()
     console.log("received message 1....................")
 }).on('data', function (message) {
     console.log('Received message from partition 0:');
@@ -27,13 +27,13 @@ consumer1.on("ready", function () {
 consumer1.on('event.log', function (log) {
     console.log("Consumer log ", log);
 });
-// consumer2.on("ready", function () {
-//     console.log("received message 2....................")
-//     consumer2.subscribe(['ritul-singh']);
-//     consumer2.consume();
-// }).on('data', function (data) {
-//     console.log('Received message from partition 1: ' + data.value);
-// });
+consumer2.on("ready", function () {
+    console.log("received message 2....................")
+    consumer2.subscribe(['new-topic']);
+    consumer2.consume();
+}).on('data', function (data) {
+    console.log('Received message from partition 1: ' + data.value);
+});
 // consumer3.on("ready", function () {
 //     console.log("received message 3....................")
 //     consumer3.subscribe(['ritul-singh']);
@@ -43,6 +43,6 @@ consumer1.on('event.log', function (log) {
 // });
 
 consumer1.connect();
-// consumer2.connect();
+consumer2.connect();
 // consumer3.connect();
 
