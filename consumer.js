d462a9e7b69beecbab99c35cc9961e48af1b1eb8
Neha-mtalkz <neha.yadav@mtalkz.com>
2023-01-16 12:39:26 +0530

    practice with two consumer

--- consumer.js
+++ consumer.js
@@ -2,12 +2,12 @@ const kafka = require('node-rdkafka');
 
 const consumer1 = new kafka.KafkaConsumer({
     "metadata.broker.list": "127.0.0.1:9092",
-    "group.id": "hello-group",
+    "group.id": "hello-group2",
     "auto.offset.reset": "earliest"
 });
 const consumer2 = new kafka.KafkaConsumer({
     "metadata.broker.list": "127.0.0.1:9092",
-    "group.id": "hello-group",
+    "group.id": "hello-group1",
     "auto.offset.reset": "earliest"
 });
 
@@ -17,22 +17,24 @@ const consumer2 = new kafka.KafkaConsumer({
 //     "auto.offset.reset": "earliest"
 // });
 
-consumer1.on("ready", function () {
-    consumer1.subscribe(['new-topic']);
-    consumer1.consume()
-    console.log("received message 1....................")
-}).on('data', function (message) {
-    console.log('Received message from partition 0:');
-});
-consumer1.on('event.log', function (log) {
-    console.log("Consumer log ", log);
-});
 consumer2.on("ready", function () {
     console.log("received message 2....................")
     consumer2.subscribe(['new-topic']);
     consumer2.consume();
 }).on('data', function (data) {
+    console.log(data.partition, " 2");
     console.log('Received message from partition 1: ' + data.value);
+});
+consumer1.on("ready", function () {
+    consumer1.subscribe(['new-topic']);
+    consumer1.consume()
+    console.log("received message 1....................")
+}).on('data', function (message) {
+    console.log(message.partition, " 1");
+    console.log('Received message from partition 0:', message.value);
+});
+consumer1.on('event.log', function (log) {
+    console.log("Consumer log ", log);
 });
 // consumer3.on("ready", function () {
 //     console.log("received message 3....................")
--- producer.js
+++ producer.js
@@ -10,7 +10,9 @@ producer.on('delivery-report', function (err, report) {
 });
 producer.on('ready', function () {
     console.log('Producer is ready');
-    producer.produce('new-topic', -1, Buffer.from('Hello Nehaaaaaaaaaaaaaaa Singh'), "Stormwind");
+    producer.produce('new-topic', 0, Buffer.from('Hello Neha Singh'), "Stormwind");
+    producer.produce('new-topic', 1, Buffer.from('Hello Ritul Singh'), "Stormwind");
+    // producer.produce('new-topic', 2, Buffer.from('Hello Nehaaaaaaaaaaaaaaa Singh'), "Stormwind");
 });
 producer.on('event.error', (err) => {
     console.log('Event error', err);
