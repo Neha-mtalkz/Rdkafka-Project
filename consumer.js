25581ad3b5ca4a7749ab4257e95e6d2a751139c7
Neha-mtalkz <neha.yadav@mtalkz.com>
2023-01-16 11:19:08 +0530

    first commit

---
+++ consumer.js
@@ -0,0 +1,48 @@
+const kafka = require('node-rdkafka');
+
+const consumer1 = new kafka.KafkaConsumer({
+    "metadata.broker.list": "127.0.0.1:9092",
+    "group.id": "hello-group",
+    "auto.offset.reset": "earliest"
+});
+// const consumer2 = new kafka.KafkaConsumer({
+//     "metadata.broker.list": "127.0.0.1:9092",
+//     "group.id": "myid",
+//     "auto.offset.reset": "earliest"
+// });
+
+// const consumer3 = new kafka.KafkaConsumer({
+//     "metadata.broker.list": "127.0.0.1:9092",
+//     "group.id": "myid",
+//     "auto.offset.reset": "earliest"
+// });
+
+consumer1.on("ready", function () {
+    consumer1.subscribe(['new-topic']);
+    consumer1.consume();
+    console.log("received message 1....................")
+}).on('data', function (message) {
+    console.log('Received message from partition 0:');
+});
+consumer1.on('event.log', function (log) {
+    console.log("Consumer log ", log);
+});
+// consumer2.on("ready", function () {
+//     console.log("received message 2....................")
+//     consumer2.subscribe(['ritul-singh']);
+//     consumer2.consume();
+// }).on('data', function (data) {
+//     console.log('Received message from partition 1: ' + data.value);
+// });
+// consumer3.on("ready", function () {
+//     console.log("received message 3....................")
+//     consumer3.subscribe(['ritul-singh']);
+//     consumer3.consume();
+// }).on('data', function (data) {
+//     console.log('Received message from partition 2: ' + data.value);
+// });
+
+consumer1.connect();
+// consumer2.connect();
+// consumer3.connect();
+
---
+++ producer.js
@@ -0,0 +1,21 @@
+const kafka = require('node-rdkafka');
+//Stormwind
+const producer = new kafka.Producer({
+    "metadata.broker.list": "127.0.0.1:9092",
+    "group.id": "hello-group",
+    dr_cb: true
+});
+producer.on('delivery-report', function (err, report) {
+    console.log('delivery-report: ' + JSON.stringify(report));
+});
+producer.on('ready', function () {
+    console.log('Producer is ready');
+    producer.produce('new-topic', -1, Buffer.from('Hello Nehaaaaaaaaaaaaaaa Singh'), "Stormwind");
+});
+producer.on('event.error', (err) => {
+    console.log('Event error', err);
+})
+producer.on('disconnected', function (arg) {
+    console.log('Producer disconnected. ', arg);
+});
+producer.connect();
